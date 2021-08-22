3d88e91faa880ae78314a57636cd90d8a2efd8e0
Erik Tejeda <etrq@protonmail.com>
2021-08-22 16:37:07 -0500

    Create reply model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_193114) do
+ActiveRecord::Schema.define(version: 2021_08_22_210054) do
 
   create_table "posts", force: :cascade do |t|
     t.text "comment"
@@ -19,4 +19,13 @@ ActiveRecord::Schema.define(version: 2021_08_22_193114) do
     t.string "subject"
   end
 
+  create_table "replies", force: :cascade do |t|
+    t.text "comment"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_replies_on_post_id"
+  end
+
+  add_foreign_key "replies", "posts"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.create(subject: "Nunc eget ipsum convallis,", comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat, odio eu facilisis bibendum, lorem justo imperdiet ante, eget sagittis ipsum justo eu turpis. Nullam dignissim pretium diam.")
-Post.create(subject: "fringilla elit mollis,", comment: "Praesent dignissim augue vestibulum, varius nibh id, lobortis justo.")
-Post.create(comment: "Nullam ut lobortis neque. Pellentesque in lorem et purus commodo volutpat vitae non urna. Aenean quis pretium ligula.")
-Post.create(subject: "vehicula dui.", comment: "Nam odio risus, volutpat faucibus finibus in, ullamcorper quis quam.")
-Post.create(subject: "Aliquam pretium augue elit, id finibus ante fermentum vitae.", comment: "Nullam vel augue malesuada, mollis neque a, semper velit. Integer tempor nisi vel sapien vestibulum efficitur. Quisque vehicula vestibulum mi, vitae facilisis lorem porttitor porttitor. Integer sed enim arcu.")
-Post.create(subject: "Nullam ac scelerisque enim.", comment: "In lectus odio, finibus nec eros nec, ultricies venenatis turpis.")
-Post.create(subject: "In arcu urna, sodales in mollis quis, tempor et urna. Suspendisse viverra maximus nisi.", comment: "Curabitur tempor feugiat tincidunt. Mauris sagittis, nulla id euismod congue, sem urna consequat lacus, nec dictum erat ligula ac leo. Nullam nec dui sit amet erat laoreet ornare vitae eget sapien.")
-Post.create(comment: "Ut arcu est, molestie non elit porttitor, eleifend pulvinar massa. Etiam turpis mauris, finibus vitae mi eget, eleifend facilisis leo.")
-Post.create(subject: "In ornare massa malesuada gravida viverra.", comment: "Proin felis orci, placerat ut odio eget, congue aliquet mi.")
-Post.create(subject: "Quisque vel bibendum mi.", comment: "Morbi nulla ante, consequat a eros quis, pharetra vehicula nulla.")
+phrases = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat, odio eu facilisis bibendum, lorem justo imperdiet ante, eget sagittis ipsum justo eu turpis. Nullam dignissim pretium diam.",
+  "Praesent dignissim augue vestibulum, varius nibh id, lobortis justo.",
+  "Nullam ut lobortis neque. Pellentesque in lorem et purus commodo volutpat vitae non urna. Aenean quis pretium ligula.",
+  "Nam odio risus, volutpat faucibus finibus in, ullamcorper quis quam.",
+  "Nullam vel augue malesuada, mollis neque a, semper velit. Integer tempor nisi vel sapien vestibulum efficitur. Quisque vehicula vestibulum mi, vitae facilisis lorem porttitor porttitor. Integer sed enim arcu.",
+  "In lectus odio, finibus nec eros nec, ultricies venenatis turpis.",
+  "Curabitur tempor feugiat tincidunt. Mauris sagittis, nulla id euismod congue, sem urna consequat lacus, nec dictum erat ligula ac leo. Nullam nec dui sit amet erat laoreet ornare vitae eget sapien.",
+  "Ut arcu est, molestie non elit porttitor, eleifend pulvinar massa. Etiam turpis mauris, finibus vitae mi eget, eleifend facilisis leo.",
+  "Proin felis orci, placerat ut odio eget, congue aliquet mi.",
+  "Morbi nulla ante, consequat a eros quis, pharetra vehicula nulla.",
+  "Nunc eget ipsum convallis,",
+  "fringilla elit mollis,",
+  "vehicula dui.",
+  "Aliquam pretium augue elit, id finibus ante fermentum vitae.",
+  "Nullam ac scelerisque enim.",
+  "In arcu urna, sodales in mollis quis, tempor et urna. Suspendisse viverra maximus nisi.",
+  "In ornare massa malesuada gravida viverra.",
+  "Quisque vel bibendum mi.",
+  "",
+  "",
+]
+
+10.times do
+  post = Post.create(subject: phrases.sample, comment: phrases.sample)
+  (rand * 5).round.times do
+    post.replies.create(comment: phrases.sample)
+  end
+end

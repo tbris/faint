5f6fc8b59dce0400ac65c280d59b4146331a9638
Erik Tejeda <etrq@protonmail.com>
2021-08-22 14:30:36 -0500

    Rename content column to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_035829) do
+ActiveRecord::Schema.define(version: 2021_08_22_192737) do
 
   create_table "posts", force: :cascade do |t|
-    t.text "content"
+    t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
